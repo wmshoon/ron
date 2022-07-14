9d788dda03ee921832beab82039b4b53401ffc1b
wmshoon <choidonghun0831@gmail.com>
2022-07-14 17:38:59 +0900

    sasf

---
+++ app.ts
@@ -0,0 +1,7 @@
+const colorArray = ["#40E0D0","#FF0080","#FC466B","#3F5EFB","#7303c0","#ec38bc","#fdeff9"];
+
+var lengthOfColorArray = colorArray.length;
+
+for(var i = 0; i<10; i++){
+    document.write(`<box class = "bgr" id = "bgr${i}" style = "border: ${colorArray[i]} solid 5px;"> asdf</box>`);
+}
