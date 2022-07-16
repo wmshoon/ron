e809bcfd196e39834bd13ce37c853192e93af5b2
wmshoon <choidonghun0831@gmail.com>
2022-07-16 21:01:58 +0900

    7 16

--- app.js
+++ app.js
@@ -7,20 +7,23 @@ var colorArray = [
     "#ec38bc",
     "#fdeff9",
 ];
+var Scale = function () {
+    var scaleX = document.documentElement.scrollWidth;
+    var scaleY = document.documentElement.scrollHeight;
+    alert(scaleX + "!" + scaleY + "!");
+};
 var lengthOfColorArray = colorArray.length;
-var boxWidthAndHeight = [
-    199, 143, 165, 138, 102, 124, 180, 143, 179, 200, 132, 131, 143, 143, 166,
-    196, 127, 210, 74, 114, 115, 137, 217, 83, 112, 195, 209, 231, 31, 42, 51, 85,
-    190, 30, 106, 107, 112, 204,
-];
+var boxWidthAndHeight = [7, 12, 10, 9, 12, 8, 12, 13, 13, 9, 7, 11];
 for (var i = 0; i < 12; i++) {
     document.write("<box class = \"bgr\" id = \"bgr".concat(i, "\"></box>"));
-    var targetElement = document.getElementById("bgr".concat(i));
-    targetElement.style.width = "".concat(boxWidthAndHeight[i], "px");
-    targetElement.style.height = "".concat(boxWidthAndHeight[i], "px");
-    targetElement.style.animationName = "sizeChange";
-    targetElement.style.margin = "".concat(Math.random() * Math.random() * 100, "px");
+    var targetElement_1 = document.getElementById("bgr".concat(i));
+    targetElement_1.style.width = "".concat(boxWidthAndHeight[i], "rem");
+    targetElement_1.style.height = "".concat(boxWidthAndHeight[i], "rem");
+    targetElement_1.style.animationName = "sizeChange";
+    targetElement_1.style.margin = "".concat(Math.random() * Math.random() * 100, "px");
 }
-document.write('<box id = "aboutRon">About Ron</box>');
+document.write('<box id = "aboutRon">About</box>');
+var targetElement = document.getElementById("bgr".concat(3));
 document.write("<title>About Ron</title>");
-document.write("<noscript><h1>fork you. enable JS</h1></noscript>");
+// function setPositionOfElement(positionX, positionY): any {}
+Scale();
--- app.ts
+++ app.ts
@@ -7,23 +7,30 @@ const colorArray = [
   "#ec38bc",
   "#fdeff9",
 ];
+const Scale = function () {
+  var scaleX = document.documentElement.scrollWidth;
+  var scaleY = document.documentElement.scrollHeight;
+  alert(scaleX + "!" + scaleY + "!");
+};
 
 var lengthOfColorArray = colorArray.length;
 
-const boxWidthAndHeight = [
-  199, 143, 165, 138, 102, 124, 180, 143, 179, 200, 132, 131, 143, 143, 166,
-  196, 127, 210, 74, 114, 115, 137, 217, 83, 112, 195, 209, 231, 31, 42, 51, 85,
-  190, 30, 106, 107, 112, 204,
-];
+const boxWidthAndHeight = [7, 12, 10, 9, 12, 8, 12, 13, 13, 9, 7, 11];
 
 for (var i = 0; i < 12; i++) {
   document.write(`<box class = "bgr" id = "bgr${i}"></box>`);
   const targetElement: any = document.getElementById(`bgr${i}`);
-  targetElement.style.width = `${boxWidthAndHeight[i]}px`;
-  targetElement.style.height = `${boxWidthAndHeight[i]}px`;
+  targetElement.style.width = `${boxWidthAndHeight[i]}rem`;
+  targetElement.style.height = `${boxWidthAndHeight[i]}rem`;
   targetElement.style.animationName = "sizeChange";
   targetElement.style.margin = `${Math.random() * Math.random() * 100}px`;
 }
 
-document.write('<box id = "aboutRon">About Ron</box>');
+document.write('<box id = "aboutRon">About</box>');
+const targetElement: any = document.getElementById(`bgr${3}`);
+
 document.write("<title>About Ron</title>");
+
+// function setPositionOfElement(positionX, positionY): any {}
+
+Scale();
